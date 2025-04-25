869aba229a6428fce368de35e17849c0f16a6460
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-04-25 15:40:03 +0600

    feat: added all skills component

--- src/features/homepage/index.ts
+++ src/features/homepage/index.ts
@@ -1,4 +1,5 @@
 import DetailsBento from "./DetailsBento.astro";
 import Hero from "./hero.astro";
+import Skills from "./Skills/Skills.astro";
 
-export { DetailsBento, Hero };
+export { DetailsBento, Hero, Skills };

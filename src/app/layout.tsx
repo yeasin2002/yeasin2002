8cd8ab18a6b6bade78a79ddb34fb526981cbffb4
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-08-08 10:21:47 +0600

    feat: updated new blogs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import '@/app/global.css';
+import { fontVariables } from "@/fonts";
+import { cn } from "@/lib";
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
@@ -10,7 +12,7 @@ const inter = Inter({
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
+      <body className={cn("flex flex-col min-h-screen", fontVariables)}>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
